7c3198106b218a6ec105c6cbe7f941c8bea62d51
daehyun kang <soejdtodxn@kw.ac.kr>
2020-08-10 00:58:25 +0900

    Add NotifyPropertyChangedViewModel

---
+++ mvvm-netstandard/mvvm-netstandard/ComponentModel/NotifyPropertyChangedViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace mvvm_netstandard.ComponentModel
+{
+    public class NotifyPropertyChangedViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Notify property change works only when valueToChange and value are different.
+        /// </summary>
+        protected bool SetValueWithNotify<T>(ref T valueToChange, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(valueToChange, value))
+            {
+                return false;
+            }
+            else
+            {
+                valueToChange = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+        }
+    }
+}
