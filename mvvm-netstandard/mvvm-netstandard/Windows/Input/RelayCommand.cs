b02af555071e7c1209ed5b9a3477ddc314b02312
daehyun kang <soejdtodxn@kw.ac.kr>
2020-08-10 01:17:15 +0900

    Add RelayCommand

---
+++ mvvm-netstandard/mvvm-netstandard/Windows/Input/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace mvvm_netstandard.Windows.Input
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        readonly Predicate<object> canExecute;
+        readonly Action<object> execute;
+
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+            {
+                return true;
+            }
+
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (execute == null || !canExecute(parameter))
+            {
+                return;
+            }
+
+            execute(parameter);
+        }
+    }
+}
